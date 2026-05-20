1ffa7ad0cd86a9a265789ef45fcea8781a30cb39
Sajitha Tharaka <sajithatharaka@gmail.com>
2026-05-20 10:48:20 +0100

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -5,11 +5,28 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default {
-  mode: 'production', // or 'development'
-  entry: './src/routes/index.js',
+  mode: 'production',
+  entry: './src/routes/index.tsx',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.[jt]sx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'ts-loader',
+          options: {
+            transpileOnly: true,
+          },
+        },
+      },
+    ],
   },
 };
